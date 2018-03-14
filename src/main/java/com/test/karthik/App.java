9bac330b060d1baae059fe7fe1298c598df99470
Karthik <vkartheek.ky1311@gmail.com>
2018-03-13 23:02:32 -0700

    mvn project files added

---
+++ src/main/java/com/test/karthik/App.java
@@ -0,0 +1,13 @@
+package com.test.karthik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
